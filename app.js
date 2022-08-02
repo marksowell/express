e048ee274f363b6707023b157501acf9dd10aed4
Mark Sowell <mark@marksowell.com>
2022-08-02 14:42:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 3000 
 
 app.get('/', (req, res) => {
-  res.send('Mark Sowell - Express application')
+  res.send('Mark Sowell - process.env.SECRET')
 })
 
 app.listen(port, () => {
